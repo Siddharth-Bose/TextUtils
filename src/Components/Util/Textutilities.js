d249800c8aae1efa6eba8eb5cbc63bb0494212a1
Siddharth Bose <siddharthbose95@gmail.com>
2022-08-01 23:31:08 +0530

    Added TextAreas Component

--- src/Components/Util/Textutilities.js
+++ src/Components/Util/Textutilities.js
@@ -3,28 +3,43 @@ import Button from "./Button";
 
 export default function Textutilities(props) {
   return (
-  <>
-  <div className="container mt-3">
-    <Card style={{ backgroundColor:'rgba(14, 28, 109, 0.753)'}}>
-      <div className="mb-3">
-        <textarea
-          className="form-control"
-          placeholder="Type in your text here."
-          id="floatingTextarea2"
-          rows="8"
-        ></textarea>
+    <>
+      <div className="container mt-3">
+        <Card style={{ backgroundColor: "rgba(14, 28, 109, 0.753)" }}>
+          <div className="mb-3">
+            <textarea
+              className="form-control"
+              placeholder="Type in your text here."
+              id="floatingTextarea2"
+              rows="5"
+            ></textarea>
+          </div>
+        </Card>
       </div>
-    </Card>
-  </div>
-  <div className="container mt-3 text-center">
-    <Button className="m-3">Upper Case</Button>
-    <Button className="m-3">Lower Case</Button>
-    <Button className="m-3">Word Count</Button>
-    <Button className="m-3">Copy Your Text</Button>
-    <Button className="m-3">Clear Text</Button>
-    <Button className="m-3">Sentence Case</Button>
-    <Button className="m-3">Abbreviate Name</Button>
-  </div>
-  </>
+      <div className="container mt-3 text-center">
+        <Button className="m-3">Upper Case</Button>
+        <Button className="m-3">Lower Case</Button>
+        <Button className="m-3">Word Count</Button>
+        <Button className="m-3">Copy Your Text</Button>
+        <Button className="m-3">Clear Text</Button>
+        <Button className="m-3">Sentence Case</Button>
+        <Button className="m-3">Abbreviate Name</Button>
+      </div>
+      <div className="container mt-3">
+        <Card style={{ backgroundColor: "seagreen" }}>
+          <div className="mb-3">
+            <textarea
+              className="form-control"
+              placeholder="Type in your text here."
+              id="floatingTextarea2"
+              rows="5"
+            ></textarea>
+          </div>
+        </Card>
+      </div>
+      <div className="container mt-3 text-center">
+        <Button className="m-3 fluid">Copy Converted Text</Button>
+      </div>
+    </>
   );
 }
